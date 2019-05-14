13ae66b5abe0cea531dd38874225722adcf3070e
Jacob Wright <wrig0099@stthomas.edu>
2019-05-14 20:02:12 +0000

    what is happening

--- App-1/app.R
+++ App-1/app.R
@@ -6,24 +6,13 @@ ui <- fluidPage(
    sidebarLayout(
       sidebarPanel(
         helpText("This is just filler for now."),
-<<<<<<< HEAD
-        selectInput("var", 
-                    label = "Choose a variable to display",
-                    choices = list("Select one..."
-                                   "Number of Parks", 
-                                   "Other things",
-                                   "Other things1", 
-                                   "Other things2"),
-                    selected = "Select one...")
-=======
         checkboxGroupInput("var", 
                     label = "Choose a variable to filter by:",
-                    choices = list("Unemployment Rate" = 1, 
-                                   "Health Insurance Coverage" = 2,
-                                   "Median Income" = 3, 
-                                   "Crime Rate" = 4),
+                    choices = list("Unemployment Rate", 
+                                   "Health Insurance Coverage",
+                                   "Median Income", 
+                                   "Crime Rate"),
                     selected = c(1,2,3,4))
->>>>>>> 0dd9bc48498fd929c7d3c63a2e69f8dcd6a3ff70
       ), #sidebarPanel end
       mainPanel(
         textOutput("selected_var"),
@@ -33,7 +22,13 @@ ui <- fluidPage(
 )
 #rendertable
 server <- function(input, output) {
-    #percent_map(var = data, color = colorRamps::matlab.like(n = 10), legend.title = input$var, min = input$range[1], max = input$range[2])
+  output$map <- renderPlot({
+    data <- switch(input$var, 
+                   "Unemployment Rate" = final_table$`Unemployment Rate`,
+                   "Health Insurance Coverage" = final_table$`Total Health Insurance Coverage Uninsured, Percent`
+    )
+    percent_map(var = data, color = colorRamps::matlab.like(n = 10), legend.title = input$var)
+  })
 }
 
 # Run the app ----
