cd8d7b7eb1a3441113b7362d7be466dfc2980912
glas0001 <glas0001@stthomas.edu>
2019-05-14 19:39:44 +0000

    update app

--- App-1/app.R
+++ App-1/app.R
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-#install.packages(c("maps", "mapproj", "colorRamps", "shiny", "dplyr", 
-                   "leaflet", "rgdal", "sf", "tidyverse", "readr"))
-library(maps)
-library(mapproj)
-library(colorRamps)
-library(lubridate)
-library(readxl)
-library(rgdal)
-library(sf)
-library(leaflet)
-library(dplyr)
-library(tidyverse)
-library(readr)
-source("/cloud/project/helpers.R")
-=======
-#Run prerun.R before running
->>>>>>> e0c8bedb624ab13d795a7d478aaf8b34ccab5c2c
 
 source("/cloud/project/helpers.R")
 
@@ -24,13 +6,13 @@ ui <- fluidPage(
    sidebarLayout(
       sidebarPanel(
         helpText("This is just filler for now."),
-        selectInput("var", 
-                    label = "Choose a variable to display",
-                    choices = list("Number of Parks", 
-                                   "Other things",
-                                   "Other things1", 
-                                   "Other things2"),
-                    selected = "Number of Parks")
+        checkboxGroupInput("var", 
+                    label = "Choose a variable to filter by:",
+                    choices = list("Unemployment Rate" = 1, 
+                                   "Health Insurance Coverage" = 2,
+                                   "Median Income" = 3, 
+                                   "Crime Rate" = 4),
+                    selected = c(1,2,3,4))
       ), #sidebarPanel end
       mainPanel(
         textOutput("selected_var"),
@@ -38,7 +20,7 @@ ui <- fluidPage(
       ) #mainPanel end
   )
 )
-
+#rendertable
 server <- function(input, output) {
     #percent_map(var = data, color = colorRamps::matlab.like(n = 10), legend.title = input$var, min = input$range[1], max = input$range[2])
 }
