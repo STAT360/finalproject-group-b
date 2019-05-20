8fc2819663a236a7ca7473d7c6297e2ca15cc6c0
glas0001 <glas0001@stthomas.edu>
2019-05-20 20:16:07 +0000

    We are in the end game now

--- App-1/app.R
+++ App-1/app.R
@@ -15,7 +15,7 @@ ui <- fluidPage(
                     selected = "Unemployment Rate"),
         selectInput("state",
                     label = "Choose a state to see all data:",
-                    choices = final_table$'State',
+                    choices = c("All",final_table$'State'),
                     selected = NULL)
       ), #sidebarPanel end
       mainPanel(
@@ -32,17 +32,23 @@ server <- function(input, output) {
                    "Unemployment Rate" = final_table$`Unemployment Rate Rank`,
                    "Health Insurance"= final_table$`Health Insurance Rank`,
                    "Graduation Rate" = final_table$`Graduation Rate Rank`,
-                   "Median_Income_Rank" = final_table$`Median Income Rank`,
+                   "Median Income" = final_table$`Median Income Rank`,
                    "Crime Rate"  = final_table$`Crime Rate Rank`)
     percent_map(var = data, color = rev(brewer.pal(5,"YlGn")), legend.title = input$var)
   })
   
-  output$table <- renderTable(
-    final_table %>%
-      select(`State`,input$var,paste0(input$var, " Rank")) %>% 
-      filter(`State`==input$state)
-  )
-}
+  output$table <- renderTable({
+    if(input$state == "All"){
+      final_table %>%
+        select(`State`,input$var,paste0(input$var, " Rank")) %>%
+        arrange(!!sym(paste0(input$var, " Rank")))
+    }else{
+      final_table %>%
+        select(`State`,input$var,paste0(input$var, " Rank")) %>% 
+        filter(`State`==input$state)
+    }
+  })
+}#server
 
 # Run the app ----
 shinyApp(ui = ui, server = server)
